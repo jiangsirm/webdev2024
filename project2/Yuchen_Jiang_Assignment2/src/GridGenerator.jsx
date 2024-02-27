ca68d77f13af8fa1e52bb3772af4332612e2736a
yuchen jiang <konsteintinj@yuchendeMBP.hsd1.wa.comcast.net>
2024-02-27 15:36:32 -0800

    project2Initial

---
+++ project2/Yuchen_Jiang_Assignment2/src/GridGenerator.jsx
@@ -0,0 +1,200 @@
+import { useState } from "react"
+import React from "react"
+import "./Box.css"
+import { useEffect } from "react";
+
+export function Grid() {
+    const [gridWidth, setGridWidth] = useState(20);
+    const [gridHeight, setGridHeight] = useState(20);
+    const [boxClass, setBoxClass] = useState(getRandomCells(gridWidth, gridHeight));
+    const [toggleAutoplay, setToggleAutoplay] = useState(false);
+    const [liveCell, setLiveCell] = useState(getLiveCells());
+
+    useEffect(() => {
+        let interval;
+        if (toggleAutoplay) {
+            interval = setInterval(() => {
+                setBoxClass((preGrid) => getNextGrid(gridWidth, gridHeight, preGrid));
+              }, 1000);
+        }
+        return () => clearInterval(interval);
+    }, [toggleAutoplay]);
+
+    useEffect(() => {
+        setLiveCell(getLiveCells());
+    }, [boxClass]);
+
+
+    function getLiveCells() {
+        let count = 0;
+        for (let i = 0; i < gridHeight * gridWidth; i++) {
+            if (boxClass[i] === 'box live') {
+                count += 1;
+            }
+        }
+        return count;
+    }
+
+    function resetGrid() {
+        setBoxClass(Array(40 * 40).fill('box'));
+    }
+
+    function handleAutoplay() {
+        setToggleAutoplay(prevToggle => !prevToggle);
+    }
+
+    function forwardOneFrame() {
+        setToggleAutoplay(false);
+        setBoxClass(getNextGrid(gridWidth, gridHeight, boxClass));
+    }
+
+    function getNextGrid(width, height, prevGrid) {
+        const update = [...prevGrid];
+        for (let i = 0; i < height * width; i++) {
+            let liveCount = 0;
+            if (Math.floor(i / width) > 0) {
+                if (prevGrid[i - width] === "box live") {
+                    liveCount += 1;
+                }
+                if (i % width > 0) {
+                    if (prevGrid[i - width - 1] === "box live") {
+                        liveCount += 1;
+                    }
+                }
+                
+                if (i % width < width - 1) {
+                    if (prevGrid[i - width + 1] === "box live") {
+                        liveCount += 1;
+                    }
+                } 
+            }
+
+            if (Math.floor(i / width) < height - 1) {
+                if (prevGrid[i + width] === "box live") {
+                    liveCount += 1
+                }
+
+                if (i % width > 0) {
+                    if (prevGrid[i + width - 1] === "box live") {
+                        liveCount += 1;
+                    }
+                }
+                
+                if (i % width < width - 1) {
+                    if (prevGrid[i + width + 1] === "box live") {
+                        liveCount += 1;
+                    }
+                } 
+            }
+
+            if (i % width > 0) {
+                if (prevGrid[i - 1] === "box live") {
+                    liveCount += 1;
+                }
+            }
+            
+            if (i % width < width - 1) {
+                if (prevGrid[i + 1] === "box live") {
+                    liveCount += 1;
+                }
+            }
+
+            if (prevGrid[i] === "box" && liveCount === 3) {
+                update[i] = "box live";
+            } else if (prevGrid[i] === "box live") {
+                if (liveCount < 2 || liveCount > 3) {
+                    update[i] = "box";
+                }
+            }
+        }
+        return update;
+    }
+
+    function getRandomCells(width, height) {
+        const update = Array(40 * 40).fill('box');
+        for (let i = 0; i < width * height; i++) {
+                const possibility = Math.floor( Math.random() * 20);
+                if (possibility === 0) {
+                    update[i] = update[i] === 'box' ? 'box live': 'box';
+                }
+        }
+        return update;
+    }
+    
+    function randomizeLiveCell(width, height) {
+        setToggleAutoplay(false);
+        setBoxClass(getRandomCells(width, height));
+    }
+
+    function changeWidth(event) {
+        const newWidth = parseInt(event.target.value);
+        if(isNaN(newWidth)) {
+            setGridWidth(3)
+        } else if (newWidth < 3) {
+            setGridWidth(3);
+        }  else if (newWidth > 40) {
+            setGridWidth(40);
+        } else {
+            setGridWidth(newWidth);
+        }
+        setToggleAutoplay(false);
+        setBoxClass(Array(40 * 40).fill('box'))
+    }
+
+    function changeHeight(event) {
+        const newHeight = parseInt(event.target.value);
+        if (isNaN(newHeight)) {
+            setGridHeight(3);
+        } else if (newHeight < 3) {
+            setGridHeight(3);
+        } else if (newHeight > 40) {
+            setGridHeight(40);
+        } else {
+            setGridHeight(newHeight);
+        }
+        setToggleAutoplay(false);
+        setBoxClass(Array(40 * 40).fill('box'))
+    }
+
+    function clickBox(index) {
+        if (toggleAutoplay === false) {
+            console.log("clicked")
+            const update = [...boxClass];
+            update[index] = update[index] === 'box' ? 'box live': 'box';
+            setBoxClass(update)
+        }
+    }
+
+    function renderGrid() {
+        const gridBoxes = [];
+        for (let i = 0; i < gridWidth * gridHeight; i++) {
+            gridBoxes.push(<div key={i} className={boxClass[i]} onClick={() => clickBox(i)}></div>);
+        }
+        return gridBoxes;   
+    }
+
+    return (
+        <div id="outerContainer">
+            <div id="gridOuterContainer">
+                <div id="gridContainer" style={{'--grid-height': gridHeight, '--grid-width': gridWidth}}>
+                    {renderGrid()}
+                </div>
+            </div>
+            <div className="toolBarContainer">
+                <button className="toolItem toggle" onClick={() => handleAutoplay()}>{toggleAutoplay ? "Stop": "Auto"}</button>
+                <button className="toolItem" onClick={() => randomizeLiveCell(gridWidth, gridHeight)}>Spawn Cells</button>
+                <button className="toolItem" onClick={() => resetGrid()}>Reset</button>
+                <button className="toolItem" onClick={() => forwardOneFrame()}>Next</button>
+                <label className="toolItem" htmlFor="gridWidth">
+                    Grid Width: <input type="number" id="gridWidth" name="gridWidth" value={gridWidth} onChange={changeWidth}/>
+                </label>
+                <label className="toolItem" htmlFor="gridHeight">
+                    Grid Height: <input type="number" id="gridHeight" name="gridHeight" value={gridHeight} onChange={changeHeight}/>
+                </label>
+            </div>
+            <div className="cellInfo">
+                <span>Current Live Cells: {liveCell}</span>
+            </div>
+        </div>
+    )
+}
