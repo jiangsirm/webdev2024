f9e4c2bf7aedd2c37dfc3dbdcb151f155af5b6bd
jiangsirm <jycsirm@gmail.com>
2024-03-11 23:01:39 -0700

    ChangeTexts

--- project2/Yuchen_Jiang_Assignment2/src/Credit.jsx
+++ project2/Yuchen_Jiang_Assignment2/src/Credit.jsx
@@ -2,7 +2,7 @@ import "./Credit.css"
 export default function Credit () {
     return (
         <div className="credit">
-            Made by Yuchen with some effort<br></br>
+            Made by Yuchen Jiang with some effort<br></br>
             Hope you Enjoyed!
         </div>
     )
--- project2/Yuchen_Jiang_Assignment2/src/GameIntro.jsx
+++ project2/Yuchen_Jiang_Assignment2/src/GameIntro.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import "./GameIntro.css"
 
 export function GameIntro() {
-    const content = "AppleConway's Game of Life is a cellular automaton devised by mathematician John Conway. It's a zero-player game, meaning its evolution is determined by its initial state, requiring no further input. The game takes place on a grid of cells, where each cell can be alive or dead. Through a set of simple rules based on the number of neighboring live cells, the grid evolves from one generation to the next. Despite its simplicity, the Game of Life exhibits complex emergent behavior, making it a fascinating subject of study in mathematics and computer science. Here are some specific rules for the game:"
+    const content = "Conway's Game of Life is a cellular automaton devised by mathematician John Conway. It's a simulation rather than a game, meaning the evolution of the cells is determined by its initial state, requiring no further input. The game takes place on a grid of cells, where each cell can be alive or dead. Through a set of simple rules based on the number of neighboring live cells, the grid evolves from one generation to the next. Despite its simplicity, the Game of Life exhibits complex behavior, making it a fascinating subject of study in mathematics and computer science. Here are the rules for the game:"
     let introBlcok = <div className="introBlock">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{content}</div>
     let headtingText = <h1>Introduction to Conway's Game of Live</h1>
 
@@ -10,13 +10,13 @@ export function GameIntro() {
         return (
             <div>
                 <ul>
-                    <h3>For a space that is populated:</h3>
+                    <h3>For a space that is populated in one evolution:</h3>
                     <li>Each cell with one or no neighbors dies, as if by solitude.</li>
                     <li>Each cell with four or more neighbors dies, as if by overpopulation.</li>
                     <li>Each cell with two or three neighbors survives.</li>
                 </ul>
                 <ul>
-                    <h3>For a space that is empty or unpopulated:</h3>
+                    <h3>For a space that is empty in one evolution:</h3>
                     <li>Each cell with three neighbors becomes populated</li>
                 </ul>
             </div>
